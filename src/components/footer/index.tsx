a2db59446540ed67f00bc7c2835a5e51d9498ea6
euotiniel <otonielemanuel21@gmail.com>
2023-08-29 17:45:08 +0100

    footer...

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Container from '../container'
 
 export default function Footer() {
   return (
       <footer className='bg-zinc-800 px-[65px] py-[50px]'>
         <div className='flex flex-row justify-between'>
-        <div>
-          <h4>Logotipo</h4>
+        <div className='flex items-center'>
+          <h4>Logotipo da Kudika</h4>
         </div>
         <div>
-          <h4>Mapa do site</h4>
-          <ul>
+          <h4 className='text-gray-100 text-2xl font-extrabold uppercase mb-6'>Mapa do site</h4>
+          <ul className='flex flex-col text-gray-200 text-lg font-bold uppercase gap-3'>
             <li>A Kudika</li>
             <li>Soluções</li>
             <li>Cases</li>
@@ -20,25 +19,27 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <h4>Contactos</h4>
+          <h4 className='text-gray-100 text-2xl font-extrabold uppercase mb-6'>Contactos</h4>
+          <div  className='flex flex-col gap-4 text-gray-200'>
           <div>
-            <h5>Nossa sede</h5>
-            <span>Rangel-CTTs, Ed. Digital.ao, 2º Andar</span>
+            <h5 className='text-lg font-bold uppercase'>Nossa sede</h5>
+            <span className='text-base font-light'>Rangel-CTTs, Ed. Digital.ao, 2º Andar</span>
           </div>
           <div>
-            <h5>Telefone</h5>
+            <h5 className='text-lg font-bold uppercase'>Telefone</h5>
             <span>+244 937 411 079 | 990 411 079</span>
           </div>
           <div>
-            <h5>E-mail</h5>
+            <h5 className='text-lg font-bold uppercase'>E-mail</h5>
             <span>contacto@kudikadigital.com</span>
           </div>
           <div>
-            <h5>Redes sociais</h5>
+            <h5 className='text-lg font-bold uppercase'>Redes sociais</h5>
+          </div>
           </div>
         </div>
         </div>
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center text-gray-300 mt-20'>
           <p>&copy; 2023 Kudika Digital</p>
         </div>
     </footer>
