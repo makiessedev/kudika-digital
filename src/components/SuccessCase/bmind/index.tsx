98785a496e545b07e285dde66d1b8059ec9cd964
Meick Mora <meickmora9@gmail.com>
2023-09-18 14:45:02 +0100

    test

--- src/components/SuccessCase/bmind/index.tsx
+++ src/components/SuccessCase/bmind/index.tsx
@@ -3,47 +3,50 @@ import Image from 'next/image'
 export function Bmind() {
   return (
     <>
-      <section className="mt-10 space-y-6">
-        <div className="space-y-4">
-          <h3 className="text-center text-xl font-bold uppercase text-gray-500">
-            bminds - brilliant minds
-          </h3>
-          <Image
-            className="md:hidden"
-            src="/success-case-bmind.png"
-            width={683}
-            height={365}
-            alt="bmind"
-          />
-          <div className="space-y-2">
-            <p className="text-center text-base font-normal text-gray-500">
-              O BMinds é um case de sucesso excepcional, onde nós Kudika Digital
-              tivemos um papel fundamental em seu lançamento bem-sucedido.
-            </p>
-            <p className="text-center text-base font-normal text-gray-500">
-              Com foco em estratégia, UI/UX, design visual (IDV), mídia social e
-              website, trabalhamos para criar uma experiência única e impactante
-              para os participantes.
-            </p>
+      <section className="mt-10 space-y-6 md:space-y-20">
+        <div className="space-y-4 md:flex md:space-x-10">
+          <div className="space-y-8 md:my-auto">
+            <h3 className="text-center text-xl font-bold uppercase text-gray-500 md:text-left md:text-3xl">
+              bminds - brilliant minds
+            </h3>
+            <Image
+              className="md:hidden"
+              src="/success-case-bmind.png"
+              width={683}
+              height={365}
+              alt="bmind"
+            />
+            <div className="space-y-2 md:w-[461px]">
+              <p className="text-center text-base font-normal text-gray-500 md:text-left">
+                O BMinds é um case de sucesso excepcional, onde nós Kudika
+                Digital tivemos um papel fundamental em seu lançamento
+                bem-sucedido.
+              </p>
+              <p className="text-center text-base font-normal text-gray-500 md:text-left">
+                Com foco em estratégia, UI/UX, design visual (IDV), mídia social
+                e website, trabalhamos para criar uma experiência única e
+                impactante para os participantes.
+              </p>
+            </div>
           </div>
           <Image
             className="hidden md:block"
             src="/success-case-bmind.png"
-            width={683}
-            height={365}
+            width={623}
+            height={345}
             alt="bmind"
           />
         </div>
-        <h3 className="text-center text-xl font-bold uppercase text-gray-500">
+        <h4 className="text-center text-3xl font-bold uppercase text-gray-500">
           estratégia
-        </h3>
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <p className="text-center text-base font-normal text-gray-500">
+        </h4>
+        <div className="space-y-4 md:flex md:flex-row-reverse md:space-x-10">
+          <div className="space-y-2 md:my-auto">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               Nossa equipe de estratégia desenvolveu uma visão clara e objetivos
               específicos para o evento.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               Mapeamos os principais públicos-alvo e definimos uma estratégia
               abrangente para alcançar e engajar um amplo público de
               empreendedores, profissionais e entusiastas de tecnologia em
@@ -59,23 +62,23 @@ export function Bmind() {
           />
         </div>
 
-        <div>
+        <div className="flex-col items-center md:flex">
           <h5 className="text-center text-xl font-bold uppercase text-gray-500">
             UI/UX e Design Visual
           </h5>
-          <p className="text-center text-center">
+          <p className="text-center md:text-left">
             Experiência do Usuário/Interface do Usuário & Identidade Visual
           </p>
         </div>
 
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <p className="text-center text-base font-normal text-gray-500">
+        <div className="space-y-4 md:flex md:space-x-10">
+          <div className="space-y-2 md:my-auto">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               A equipe de design da Kudika Digital concentrou-se em criar uma
               identidade visual única para o BMinds, que se refletiu em todos os
               aspectos do evento.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               Desde o logo até a identidade visual completa, nossos designers
               criaram uma experiência de marca consistente e envolvente,
               transmitindo os valores do BMinds em todos os materiais e
@@ -91,22 +94,22 @@ export function Bmind() {
           />
         </div>
 
-        <h5 className="text-center text-xl font-bold uppercase text-gray-500">
+        <h5 className="text-center text-3xl font-bold uppercase text-gray-500">
           Social Mídia
         </h5>
 
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <p className="text-center text-base font-normal text-gray-500">
+        <div className="space-y-4 md:flex md:flex-row-reverse md:space-x-10">
+          <div className="space-y-2 md:my-auto">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               A divulgação nas redes sociais foi uma parte crucial do sucesso do
               BMinds.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               Nossa equipe de mídia social criou campanhas estratégicas e
               envolventes, usando várias plataformas para promover o evento,
               compartilhar informações relevantes e estimular a participação.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               O BMinds se tornou uma sensação nas redes sociais, alcançando
               milhares de pessoas e gerando interesse crescente ao longo do
               tempo.
@@ -121,21 +124,21 @@ export function Bmind() {
           />
         </div>
 
-        <h5 className="text-center text-xl font-bold uppercase text-gray-500">
+        <h5 className="text-center text-3xl font-bold uppercase text-gray-500">
           website
         </h5>
 
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <p className="text-center text-base font-normal text-gray-500">
+        <div className="space-y-4 md:flex md:space-x-10">
+          <div className="space-y-2 md:my-auto">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               O website do BMinds foi uma peça central para o sucesso do evento.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               Nossa equipe de desenvolvimento trabalhou duro para criar um
               website responsivo, informativo e atrativo, com informações sobre
               os palestrantes, programação, atividades e inscrições.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               O website se tornou um ponto focal para os participantes e ajudou
               a fortalecer a marca BMinds como a maior plataforma de networking
               em Angola.
@@ -150,18 +153,18 @@ export function Bmind() {
           />
         </div>
 
-        <h5 className="text-center text-xl font-bold uppercase text-gray-500">
+        <h5 className="text-center text-3xl font-bold uppercase text-gray-500">
           resultados
         </h5>
 
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <p className="text-center text-base font-normal text-gray-500">
+        <div className="space-y-4 md:flex md:flex-row-reverse md:space-x-10">
+          <div className="space-y-2 md:my-auto">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               O evento se tornou um marco de sucesso, superando as expectativas
               em número de participantes, engajamento e impacto nas redes
               sociais.
             </p>
-            <p className="text-center text-base font-normal text-gray-500">
+            <p className="text-center text-base font-normal text-gray-500 md:text-left">
               O BMinds não apenas reforçou sua posição como referência em
               inovação e tecnologia em Angola, mas também estabeleceu-se como
               uma plataforma importante para impulsionar o ecossistema de
