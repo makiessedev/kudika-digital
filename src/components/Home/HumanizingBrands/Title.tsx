21536983fa0f3bd38941cfb23e8f5ffa052576c2
meickmora <meickmora9@gmail.com>
2023-09-21 12:22:51 +0100

    feat: style: fix humanizing-brands title responsive

--- src/components/Home/HumanizingBrands/Title.tsx
+++ src/components/Home/HumanizingBrands/Title.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export function Title({ children }: { children: ReactNode }) {
   return (
-    <h1 className="text-center font-montserrat text-3xl font-black uppercase text-gray-100 md:text-left md:text-5xl xl:text-6xl">
+    <h1 className="text-center font-montserrat text-3xl font-black uppercase text-gray-100 md:text-left md:text-4xl xl:text-6xl">
       {children}
     </h1>
   )
