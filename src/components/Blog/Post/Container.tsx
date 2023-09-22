0ee9189733b47400d585e1ca6903b171818fcc33
Meick Mora <meickmora9@gmail.com>
2023-09-22 13:23:47 +0100

    style: view post resize

--- src/components/Blog/Post/Container.tsx
+++ src/components/Blog/Post/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export function Container({ children }: { children: ReactNode }) {
   return (
-    <section className="px-6 pt-28 md:flex md:flex-col lg:px-20">
+    <section className="max-w-4xl px-6 pt-28 md:mx-auto md:flex md:flex-col lg:px-20">
       {children}
     </section>
   )
