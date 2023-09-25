b429e010a58ca95fad13abf6ce03faf06cee70b5
Meick Mora <meickmora9@gmail.com>
2023-09-25 13:44:20 +0100

    feat: remove slide simulator

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+/* "use client";
 
 import { SliceSimulator } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
@@ -12,3 +12,4 @@ export default function SliceSimulatorPage() {
     />
   );
 }
+ */
