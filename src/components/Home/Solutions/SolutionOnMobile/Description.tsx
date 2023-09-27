9bcf3757fb01c02272738aac38c7ce9b44c2acf3
Meick Mora <meickmora9@gmail.com>
2023-09-27 14:41:37 +0100

    style: redifine breakpoints

--- src/components/Home/Solutions/SolutionOnMobile/Description.tsx
+++ src/components/Home/Solutions/SolutionOnMobile/Description.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react'
 
 export function Description({ children }: { children: ReactNode }) {
   return (
-    <p className="text-gray-400 w-[350px] text-center text-base font-normal">{children}</p>
+    <p className="text-gray-400 w-[310px] mx-auto text-center text-sm font-normal">{children}</p>
   )
 }
