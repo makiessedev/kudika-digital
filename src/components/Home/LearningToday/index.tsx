1cbb64d8cffb67081da0b2c9d3bff28b46942749
Meick Mora <meickmora9@gmail.com>
2023-09-27 14:03:17 +0100

    refactor: home lastTreePosts

--- src/components/Home/LearningToday/index.tsx
+++ src/components/Home/LearningToday/index.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
 import { Paragraph } from './Paragraph'
-import { Title } from './Title'
-import { redirect } from 'next/navigation'
-import { Post } from './Post'
+import Link from 'next/link'
 import { createClient } from '@/prismicio'
+import { PostsWrapper } from '@/components/Blog/Posts/PostsWrapper'
+import { PostContainer } from '@/components/Blog/Posts/PostContainer'
+import { FooterContainer } from '@/components/Blog/Posts/FooterContainer'
+import { Button } from '@/components/Blog/Posts/Button'
+import { asLink, asText } from '@prismicio/helpers'
+import { readingRate } from '@/Helpers/ReadingRate'
+import { Content } from '@/components/Blog/Posts/Content'
+import { Title } from '@/components/Blog/Posts/Title'
 
 type PostProps = {
   props: {
@@ -17,7 +23,7 @@ type PostProps = {
 
 export async function LearningToday() {
   const prismic = createClient()
-  const lastThreePosts = await prismic.getAllByType('post', {
+  const lastThreePostsRaw = await prismic.getAllByType('post', {
     limit: 3,
     orderings: [
       {
@@ -27,17 +33,63 @@ export async function LearningToday() {
     ]
   })
 
+  const lastThreePosts = lastThreePostsRaw.map((post) => {
+    return {
+      uid: post.uid,
+      title: String(post.data.title),
+      coverUrl: String(post.data.cover.url),
+      description: String(post.data.description),
+      content: asText(post.data.content),
+      readingRateInMinuts: readingRate(asText(post.data.content)),
+      author: String(post.data.author),
+      authorUrl: asLink(post.data.authorurl),
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt', {
+        day: '2-digit',
+        month: 'short',
+        year: '2-digit'
+      })
+    }
+  })
+
   return (
     <section className="mb-6 mt-8 space-y-4 px-6 py-10 lg:px-20">
       <div className="flex flex-col items-center gap-4">
-        <Title>aprendizado de hoje</Title>
+        <h2 className="text-center font-montserrat text-3xl font-black uppercase text-red-500 md:text-left md:text-5xl xl:text-6xl">
+          aprendizado de hoje
+        </h2>
         <Paragraph>Veja os últimos textos da equipe kudika digital</Paragraph>
       </div>
-      <section className="md:grid md:grid-cols-2 lg:grid-cols-3">
+      <PostsWrapper>
         {lastThreePosts.map((post) => (
-          <Post key={post.id} props={post} />
+          <PostContainer key={post.uid}>
+            <Image
+              src={String(post.coverUrl)}
+              width={595}
+              height={399}
+              alt="cover"
+            />
+            <Title>
+              {String(post.title).length > 30
+                ? `${String(post.title).substring(0, 30)}...`
+                : post?.title}
+            </Title>
+            <Content>
+              {post.content?.length > 200
+                ? `${post.content.substring(0, 200)}...`
+                : post?.content}
+            </Content>
+            <FooterContainer>
+              <span>{post.author}</span>|
+              <span>{post.updatedAt}</span>|
+              <span>Leitura: {post.readingRateInMinuts} min</span>
+            </FooterContainer>
+
+            <button className="rounded-3xl border border-gray-500/50 bg-white px-5 py-1 text-sm text-gray-500/50">
+              <Link href={`/blog/post/${post.uid}`}>Ler mais</Link>
+            </button>
+          </PostContainer>
         ))}
-      </section>
+      </PostsWrapper>
     </section>
   )
 }
