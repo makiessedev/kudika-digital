aff1465cb156e702c950cb716c9406494bf22628
Makiesse Morais <makiesse.dev@gmail.com>
2023-09-28 14:53:31 +0100

    feat: blog text spaces

--- src/components/Blog/Post/Content.tsx
+++ src/components/Blog/Post/Content.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react'
 
 export function Content({content}: {content: any}) {
   return (
-    <div dangerouslySetInnerHTML={{ __html: content }} className="text-base font-normal text-gray-500/90 md:text-base" />
+    <div dangerouslySetInnerHTML={{ __html: content }} className="text-base space-y-6 font-normal text-gray-500/90 md:text-base" />
   )
 }
