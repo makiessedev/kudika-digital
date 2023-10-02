780b8c73b22e1bcc4c4609ce75121322794464c1
Makiesse Morais <makiesse.dev@gmail.com>
2023-10-02 08:49:15 +0100

    config: added prismic revalidate

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -32,7 +32,7 @@ export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
     accessToken: process.env.PRISMIC_API_KEY,
     fetchOptions:
       process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "no-store" }
+        ? { next: { revalidate: 60 * 15} }
         : { next: { revalidate: 5 } },
     ...config,
   });
